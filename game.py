86f42a70a1ef447f92dcf4c53f6247e296e48773
Serg Kruglov <kruser@yandex.ru>
2023-04-13 10:39:59 +0000

    add fire

--- game.py
+++ game.py
@@ -1,61 +1,79 @@
 import time
 import asyncio
 import curses
+from random import randint, choice
 
 
-class EventLoopCommand():
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
 
-    def __await__(self):
-        return (yield self)
+    row, column = start_row, start_column
 
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
 
-class Sleep(EventLoopCommand):
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
 
-    def __init__(self, seconds):
-        self.seconds = seconds
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 1 < row < max_row and 1 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
+        for _ in range(randint(1, 20)):
+            await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for _ in range(20):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for _ in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
 def draw(canvas):
     curses.curs_set(False)
-    row, column = (5, 20)
     canvas.border()
+    rows, columns = canvas.getmaxyx()
 
     courutines = []
-    for item in range(5):
-        courutines.append(blink(canvas, row, column + item*2, '*'))
+    for item in range(300):
+        courutines.append(blink(canvas, randint(1, rows-2), randint(1, columns-2), choice('+*.:')))
+    courutines.append(fire(canvas, rows-2, columns/2))
 
     while True:
         for courutine in courutines.copy():
-            courutine.send(None)
-        canvas.refresh()
-        time.sleep(2)
-        for courutine in courutines.copy():
-            courutine.send(None)
-        canvas.refresh()
-        time.sleep(0.3)
-        for courutine in courutines.copy():
-            courutine.send(None)
-        canvas.refresh()
-        time.sleep(0.5)
-        for courutine in courutines.copy():
-            courutine.send(None)
-        canvas.refresh()
-        time.sleep(0.3)
+            try:
+                courutine.send(None)
+            except StopIteration:
+                courutines.pop(-1)
+            canvas.refresh()
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
